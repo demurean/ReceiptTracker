72f1d3293a834b88c4a830fc3aa279c793b57507
demurean <arielle.felicia@gmail.com>
2026-07-07 19:05:02 -0700

    established header and appendage

--- TestConnection.py
+++ TestConnection.py
@@ -1,5 +1,6 @@
 from google.oauth2.service_account import Credentials
 import gspread
+from datetime import datetime
 
 # Loads service account credentials
 # reference: https://googleapis.dev/python/google-auth/latest/reference/google.oauth2.service_account.html#module-google.oauth2.service_account
@@ -12,7 +13,26 @@ credentials = Credentials.from_service_account_file('PythonToSheetsServiceAccoun
 # Opens gsheet
 # reference: https://docs.gspread.org/en/latest/oauth2.html#for-bots-using-service-account
 gc = gspread.authorize(credentials)
-wks = gc.open_by_url('https://docs.google.com/spreadsheets/d/1LFKsn1_iR_rhZqDz8zxeU924k5FYKNnbuNPX_pRwr68/edit?gid=0#gid=0').sheet1
+SpreadSheet = gc.open_by_key('1LFKsn1_iR_rhZqDz8zxeU924k5FYKNnbuNPX_pRwr68')
 
-# # updates a row
-wks.update_acell('A1', 'Hello World')
+##-------------------------------------------------------------------------------------------------
+# finds worksheet by name 
+    # -- if NF, creates it with default headers
+    # -- if F, leaves alone,
+    # returns the worksheet
+def GetOrCreateSheet(spreadsheet, name):
+    try:
+        worksheet = spreadsheet.worksheet(name)
+    except gspread.exceptions.WorksheetNotFound as shNF:
+        print("Oops, no worksheet found by that name.", shNF)
+        worksheet = spreadsheet.add_worksheet(title=name, rows=100, cols=20)
+        # header installation
+        worksheet.update_acell('A1', 'Date')
+        worksheet.update_acell('B1', 'WIP')
+        worksheet.update_acell('C1', 'Bank Statement for:')
+        worksheet.update_acell('D1', 'WIP')
+    return worksheet
+
+wks = GetOrCreateSheet(SpreadSheet, "BankTransactions")
+# reference: https://stackoverflow.com/questions/67082749/append-a-new-row-to-the-end-of-sheets-using-gspread
+wks.append_row(['Hello', 'World'], table_range="A1:B1")
